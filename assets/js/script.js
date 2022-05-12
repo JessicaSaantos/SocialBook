4f96f1169b020219b3a34e1a8e5c94dd3e0f546b
Jessica <98659450+JessicaSaantos@users.noreply.github.com>
2022-05-12 19:10:41 -0300

    add JS
    adicionando funcionalidade de criar postagem com JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,18 @@
-function  createPostContainer (msg){
-    const main = document.querySelector('.cont_principal')
+ /*=========================ESTRUTURANDO O POST PARTE DO HTML=========================================*/
+ 
+ function  createPostContainer (msg){
+
+    /* Criar e organizar os conteúdos do post */
+    const postCreator = document.querySelector('.post_creator')
     const postContainer = document.createElement("div")
     postContainer.classList.add("post_container")
     
     const profileDiv = createProfileRow();
-    const postDescription = createPostDescription(msg)
+    const postDescription = createPostDescription(msg);
+    const postFooter = createPostFooter()
 
-    postContainer.append(profileDiv , postDescription);
-    main.append(postContainer)
-}
-
-
-function createPostDescription (msg){
-    const prg = document.createElement("p")
-    prg.classList.add("post_text")
-
-    prg.innerText = msg
-
-    return prg
+    postContainer.append(profileDiv , postDescription , postFooter);
+    postCreator.after(postContainer)
 }
 
 function createProfileRow() {
@@ -53,4 +48,94 @@ function createProfileRow() {
     return postRow;
    
 }
+
+function createPostDescription (msg){
+    const prg = document.createElement("p")
+    prg.classList.add("post_text")
+
+    prg.innerText = msg
+
+    return prg
+}
+
+function createPostFooter() {
+    const postRow = document.createElement('div')
+    postRow.classList.add('post_row')
+
+    const numbers = createPostNumber()
+    const icons = createPostIcons()
+
+    postRow.append(numbers , icons)
+
+    return postRow;
+}
+
+function createPostNumber() {
+    const number = document.createElement('div');
+    number.classList.add("numbers")
+
+    const numberLeft = document.createElement('div')
+    numberLeft.classList.add("numbers_left")
+
+    const numberRight = document.createElement('div')
+    numberRight.classList.add("numbers_right")
+
+    const spanComents = document.createElement('span')
+    const spanCompartilhamentos = document.createElement('span')
+
+    spanComents.innerText = "0 Comments"
+    spanCompartilhamentos.innerText = "0 Shares"
+
+    numberRight.append(spanComents , spanCompartilhamentos)
+    number.append(numberLeft , numberRight)
+
+    return number
+}
+
+function createPostIcons() {
+    const divLikes = document.createElement('div')
+    const iconLike = document.createElement('img')
+    iconLike.src = "./assets/img/like.png"
+
+    divLikes.append(iconLike)
+    divLikes.innerHTML = divLikes.innerHTML + "Like"
+
+    const divComment = document.createElement('div')
+    const iconComment = document.createElement('img')
+    iconComment.src = "./assets/img/comments.png"
+
+    divComment.append(iconComment)
+    divComment.innerHTML = divComment.innerHTML + "Comments"
+
+    const divShare = document.createElement('div')
+    const iconShare = document.createElement('img')
+    iconShare.src = "./assets/img/share.png"
+
+    divShare.append(iconShare)
+    divShare.innerHTML = divShare.innerHTML + "Shares"
+    
+
+    const atividadeIcons = document.createElement('div')
+    atividadeIcons.classList.add('atividade_icons')
+
+    atividadeIcons.append(divLikes , divComment , divShare)
+
+    return atividadeIcons;
+}
+
+
+ /*=========================CAPTANDO A MENSAGEM DO USUARIO PARA CRIAR O POST=========================================*/
+
+const postInput = document.querySelector('#post_input')
+postInput.addEventListener('keydown' , function(evento){
+    //evento.code for igual a "Enter"
+    //evento.code for igual a "NumpadEnter" terá que criar um post
+
+    if (evento.code === "Enter" || evento.code === "NumpadEnter") {
+        const msgUsuario = postInput.value
+        createPostContainer(msgUsuario)
+
+        postInput.value = ""
+    }
+})
 
